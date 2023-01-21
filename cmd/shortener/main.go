cfd5909b120cf92a8fd01d18121ca8e5811f34b7
Ivan Podgorny <ivanpodgorny@yandex.ru>
2023-01-22 00:54:07 +0300

    Инкремент 1 - добавлен метод создания короткого url - добавлен метод получения исходного url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"github.com/ivanpodgorny/urlshortener/cmd/shortener/router"
+	"github.com/ivanpodgorny/urlshortener/cmd/shortener/storage"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var (
+		r = router.NewRouter()
+		s = NewShortener(storage.NewMemory())
+		h = NewHandler(s)
+	)
+
+	r.Add(http.MethodPost, "/", h.Create)
+	r.Add(http.MethodGet, `/[A-Za-z0-9_-]+`, h.Get)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
---
+++ cmd/shortener/router/router.go
@@ -0,0 +1,87 @@
+package router
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+type Router struct {
+	routes map[string][]patternHandler
+}
+
+func NewRouter() *Router {
+	return &Router{
+		routes: map[string][]patternHandler{},
+	}
+}
+
+func BadRequest(w http.ResponseWriter) {
+	http.Error(w, "400 bad request", http.StatusBadRequest)
+}
+
+func NotAllowed(w http.ResponseWriter) {
+	http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+}
+
+type HandlerFunc func(http.ResponseWriter, *http.Request, ...string)
+
+type patternHandler struct {
+	method  string
+	handler HandlerFunc
+}
+
+var (
+	errNotAllowed = errors.New("not allowed")
+	errNotFound   = errors.New("not found")
+)
+
+func (r Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	handler, params, err := r.findHandler(req.URL.Path, req.Method)
+	if err == nil {
+		handler(w, req, params...)
+	} else {
+		if errors.Is(err, errNotFound) {
+			http.NotFound(w, req)
+		} else if errors.Is(err, errNotAllowed) {
+			NotAllowed(w)
+		} else {
+			BadRequest(w)
+		}
+	}
+}
+
+func (r Router) Add(method string, pattern string, handler HandlerFunc) {
+	preparedPattern := r.preparePattern(pattern)
+	r.routes[preparedPattern] = append(r.routes[preparedPattern], patternHandler{
+		method:  method,
+		handler: handler,
+	})
+}
+
+func (r Router) findHandler(url string, method string) (HandlerFunc, []string, error) {
+	for p, handlers := range r.routes {
+		matched, err := regexp.MatchString(p, url)
+		if err != nil || !matched {
+			continue
+		}
+
+		for _, h := range handlers {
+			if h.method == method {
+				re := regexp.MustCompile(p)
+
+				return h.handler, re.FindStringSubmatch(url)[1:], nil
+			}
+		}
+
+		return nil, []string{}, errNotAllowed
+	}
+
+	return nil, []string{}, errNotFound
+}
+
+func (r Router) preparePattern(pattern string) string {
+	return fmt.Sprintf(`^\/(%s)$`, strings.Trim(pattern, "/"))
+}
